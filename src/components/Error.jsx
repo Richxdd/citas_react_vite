7fd9a7ece337c9d1ab374cd7ee878f0de94bd1e5
icharrd <ricardosv46@gmail.com>
2021-12-08 19:20:38 -0500

    deploy

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = ({children}) => {
+    return (
+        <div className='bg-red-600 text-center text-white p-2 uppercase font-bold mb-3 rounded-md'>
+                    {children}
+        </div>
+    )
+}
+
+export default Error
